8fe963bd3247ad66fd26f38e49aa304e21bfb8ee
Ubuntu <mejwaller@mejwaller.mejwaller2707.a3.internal.cloudapp.net>
2018-01-26 14:05:06 +0000

    method to ensure images are all same size (as that of smallest image)

---
+++ resizeimages.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+from PIL import Image
+
+inpath="/home/mejwaller/dunpics/"
+outpath="/home/mejwaller/normalizedpics/"
+
+files=os.listdir(inpath)
+#print files
+
+for f in files:
+    print f
+    i=Image.open(inpath+f)
+    if i.size != (2592,1935):
+        print "resizing"
+        i = i.resize((2592,1935))
+    else:
+        print "Not resizing"
+
+    print "Saving " + f + " to " + outpath
+    i.save(outpath+f)
+
+
+

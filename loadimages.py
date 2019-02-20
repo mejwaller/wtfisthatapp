03f2d6d7d9e7582e15509975c3f0cd2e296afaf6
mejwaller <mwaller@ymail.com>
2019-02-20 22:01:02 +0000

    Load images into array

--- loadimages.py
+++ loadimages.py
@@ -8,7 +8,10 @@ import numpy as np
 #//resulting in an array of n images (n=number of images in imagelabels.txt) x 2592 x 1935 
 #//and a matching array with the image index in teh same position
 
-imgarray=[]
+#array to hold the image arrays...
+#imgarray=np.empty(shape=(0,1935,2592,3))
+#apparently quicker to append arrays to list then comvert that: (or find a way to vectorize...)
+imlist = []
 indarray=[]
 
 i=0;
@@ -16,23 +19,25 @@ i=0;
 with open("./imglabels.txt") as imlabels:
     reader=csv.reader(imlabels,delimiter=",")
     for row in reader:
-        #print row
+        print row, i
 	img=row[0]
 	index = row[1]
 	label = row[2]
 	img_path = "./normalizedpics/" + img + ".JPG"
 	im = Image.open(img_path)
-	imgarray.append(im)
+	imar = np.asarray(im)	
+	#add the imagarry to the array holding the images...
+	#imgarray = np.append(imgarray,[imar],axis=0)
+	imlist.append(imar)
 	indarray.append(index)
 	i+=1
 
-iar = np.asarray(imgarray[1])
+imgarray=np.asarray(imlist)
+#shape is (number of images) x 1935 (height) x 2592 (width) x 3 (RGB)
+print imgarray.shape
 
-print iar
 
-#shape is 1935 (height) x 2592 (width) x 3 (RGB)
-print iar.shape
-print imgarray[1]
+
 
 
 

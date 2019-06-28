21d1c066c3c60f8ce86c23ec87818698032e1ab2
Ubuntu <mejwaller@mejwaller.mejwaller2707.a3.internal.cloudapp.net>
2019-06-28 14:06:52 +0000

    More pics and attemtp at pickle

--- loadimages.py
+++ loadimages.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import csv
 import numpy as np
+import pickle
 
 #//load image from 'normalizedimages' dir and append into array
 #//add index to array of indices
@@ -39,12 +40,15 @@ with open("./imglabels.txt") as imlabels:
 	imar = np.asarray(im)	
 	#add the imagarry to the array holding the images...
 	#imgarray = np.append(imgarray,[imar],axis=0)
-	if index==curindex:
+        #set aside 15% as a test set - need enoough in train that a 15% test set can be extracted
+        #leaves question about how do we know how good it *really* is for where there's not enough i
+        #of a given speciesi in train to get a test set out?
+	if index==curindex:#imglabels.txt is ordedered by index so this condition is true only if next row is same species
             numof=numof+1
             print numof, numintest/numintrain
-            if numof<6:
+            if numof<6: #not enough for 15% (1/6 ~ 1.7)
                 numintrain=numintrain+1
-            elif numintest/numintrain < 0.15:
+            elif numintest/numintrain < 0.15:#if we already have more than 15% in test set, don;t add more, keep them for training!
                 addtotest=True
                 numintest=numintest+1
             else:
@@ -73,7 +77,16 @@ testarray=np.asarray(testlist)
 #shape is (number of images) x 1935 (height) x 2592 (width) x 3 (RGB)
 print trainarray.shape
 print testarray.shape
+#todo - store arrays (pickle?)
 
+train = open('train.pkl','wb')
+test= open('test.pkl','wb')
+
+pickle.dump(trainarray,train)
+pickle.dump(testarray,test)
+
+train.close()
+test.close()
 
 
 

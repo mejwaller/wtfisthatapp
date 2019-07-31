64396ba42ad98f0c2ed59570f84167a38d4f24e1
Ubuntu <mejwaller@mejwaller.mejwaller2707.a3.internal.cloudapp.net>
2019-07-31 14:35:37 +0000

    Parallel version of resizeimages

---
+++ resizepll.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+import multiprocessing as mp
+from PIL import Image
+
+inpath="./dunpics/"
+outpath="./normalizedpics/"
+
+files=os.listdir(inpath)
+
+def resize(file):
+    print file
+    i=Image.open(inpath+file)
+    if i.size != (2592,1935):
+        print "resizing"
+        i = i.resize((2592,1935))
+    else:
+        print "Not resizing"
+
+    print "Saving " + file + " to " + outpath
+    i.save(outpath+file)
+
+pool = mp.Pool(processes=8)
+
+results = [pool.apply_async(resize, args=(f,)) for f in files]
+output = [p.get() for p in results]
+
+print(output) 
+
+
+
+

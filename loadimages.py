63e2225ed47c2afeb6b5aab98c57c9f8c3d77dfe
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-08-30 14:41:23 +0100

    Only use imgaes where there's > 6 (so can ad to test), and only pickle and load 50 at a time to save memory

--- loadimages.py
+++ loadimages.py
@@ -15,28 +15,142 @@ import pickle
 #apparently quicker to append arrays to list then comvert that: (or find a way to vectorize...)
 trainlist = []
 testlist=[]
-trainindarray=[]
-testindarray=[]
+trainindlist=[]
+testindlist=[]
 
 i=0;
 
 #let's put 15% into test set
 #need at least 6 to start with, then need to keep track to ensure just 15% in are in test set
 
+#two changes ot make:
+# 1: Only add images ot training data if there are 6 or more of them
+# 2: Only add 50 at a time and then pickle to save havoing am assive set in memory in once.
+#Will have seperate proigram to load pickle files and append into one for when we do clasification bit.
+
+
 
 with open("./imglabels.txt") as imlabels:
     reader=csv.reader(imlabels,delimiter=",")
     curindex=0;
     numof=0;
-    numintrain = 0
+    numfortrain = 0
     numintest = 0
+    sizeoftrain=0
+    pklnum=1
     for row in reader:
         addtotest=False        
+        addtotrain=False
 	index=row[0]
 	label = row[1]
 	img = row[2]
 	img_path = "./normalizedpics/" + img + ".JPG"
-	im = Image.open(img_path)
+
+        if index==curindex:#imglabels.txt is ordedered by index so this condition is true only if next row is same species
+            numof=numof+1
+            if numof<6: #enough to put 15% in trainign set
+                addtotrain=False
+                numfortrain=numfortrain+1
+            else:
+                if numintest/numfortrain < 0.15:#if we already have more than 15% in test set, don't add more, keep them for training!
+                    addtotrain=False;
+                    addtotest=True;
+                    numintest+=1
+                else:
+                    addtotrain=True;
+                    addtotest=False;
+                    numfortrain=numfortrain+1
+
+            if addtotrain:
+                im = Image.open(img_path)
+                imar = np.asarray(im)
+                trainlist.append(imar)
+                trainindlist.append(index)
+                sizeoftrain+=1
+            elif addtotest:#remember, test array will necve get massive, so no need to split
+                im = Image.open(img_path)
+                imar = np.asarray(im)
+                testlist.append(imar)
+                testindlist.append(index)
+
+            if(sizeoftrain==50):#pickle it, and then reset trainlist and trainindarray
+                #and what if we've reached the end but sizeoftrain != 50?
+                x = 'train' + str(pklnum) + '.pkl'
+                y = 'indexes' + str(pklnum) + '.pkl'
+                train=open(x,'wb')
+                indexes=open(y,'wb')
+                trainarray=np.asarray(trainlist)
+                trainindarray=np.asarray(trainindlist)
+                pickle.dump(trainarray,train)
+                pickle.dump(trainindarray,indexes)
+                train.close()
+                indexes.close()
+                print trainarray.shape
+                print trainindarray.shape
+                print "Pickled " + x + " and " + y
+                trainlist=[]
+                trainindlist=[]
+                sizeoftrain=0
+                pklnum+=1
+        else:#new species
+            curindex=index
+            numof=1
+            numfortrain=1
+            numintest=0
+            addtotest=False
+            addtotrain=False
+
+    
+        print row, i, addtotrain, addtotest, sizeoftrain
+
+        i+=1
+
+    #now pickel any remaining test (i.e. we've finsihed reows but sizeoftrain < 50)
+    if sizeoftrain > 0:
+        x = 'train' + str(pklnum) + '.pkl'
+        y = 'indexes' + str(pklnum) + '.pkl'
+        train=open(x,'wb')
+        indexes=open(y,'wb')
+        trainarray=np.asarray(trainlist)
+        trainindarray=np.asarray(trainindlist)
+        pickle.dump(trainarray,train)
+        pickle.dump(trainindarray,indexes)
+        train.close()
+        indexes.close()
+        print trainarray.shape
+        print trainindarray.shape
+        print "Pickled " + x + " and " + y
+        trainlist=[]
+        trainindlist=[]
+        sizeoftrain=0
+        pklnum+=1
+
+
+testarray = np.asarray(testlist)
+testindarray = np.asarray(testindlist)
+
+print testarray.shape
+print testindarray.shape
+
+test=open('test.pkl','wb')
+testinds=open('testinds.pkl','wb')
+pickle.dump(testarray,test)
+pickle.dump(testindarray,testinds)
+test.close()
+testinds.close()
+
+print "Pickled test.pkl and testinds.pkl"
+
+
+            
+
+
+
+
+
+
+'''
+        im = Image.open(img_path)
 	imar = np.asarray(im)	
 	#add the imagarry to the array holding the images...
 	#imgarray = np.append(imgarray,[imar],axis=0)
@@ -71,6 +185,7 @@ with open("./imglabels.txt") as imlabels:
             trainlist.append(imar)
             trainindarray.append(index)
 	i+=1
+
 
 trainarray=np.asarray(trainlist)
 testarray=np.asarray(testlist)
@@ -116,7 +231,7 @@ train.close()
 
 print 'pickled training data.'
 
-
+'''
 
 
 

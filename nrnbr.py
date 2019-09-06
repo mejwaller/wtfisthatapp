255e5f3d801fe9de7ac560d47e565f78ac4d180e
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-09-06 16:51:33 +0100

    Code to perform nearest neighbour classiciation

--- nrnbr.py
+++ nrnbr.py
@@ -1,13 +1,61 @@
 import datutils as du
 import numpy as np
 
-X_tr, Y_tr, X_te, Y_te = du.loadData()
+class NearestNeighbour(object):
+    def __INIT__(self):
+        pass
 
-print "X_tr shape: "
-print X_tr.shape
-print "Y_tr shape: "
-print Y_tr.shape
-print "X_te shape: "
-print X_te.shape
-print "Y_te shape: "
-print Y_te.shape
+    def train(self,X,y):
+        """
+        N = number of (training) examples
+        D = 'depth' which row size of image x column size of image x 3 (r,g,b)
+        X is a N x D where each row is a (training) example. y is 1-dimension of size N (labels)
+        """
+        #the nearest neighbour classifier simply remembers all the training data
+        self.Xtr=X
+        self.ytr=y
+
+    def predict(self, X):
+        """
+        N = number of (test) examples
+        D = 'depth' which row size of image x column size of image x 3 (r,g,b)
+        X is a N x D where each row is a (test) example.
+        """
+
+        num_test=X.shape[0]
+
+        #make sure that the output type matches the input type
+        Ypred =  np.zeros(num_test, dtype=self.ytr.dtype)
+
+        #Loop over all test rows
+        for i in xrange(num_test): #https://www.geeksforgeeks.org/range-vs-xrange-python/
+            #find the nearest training image to the ith test image
+            #using the L1 distance (sum of absolute value differences)
+            print "Running test example %d" % (i)
+            #distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+            distances = np.sum(np.square(self.Xtr - X[i,:]), axis=1)
+            min_index = np.argmin(distances) #get the index with the smallest distance
+            Ypred[i] = self.ytr[min_index] #predict the lable of the nearest example
+
+        return Ypred
+
+Xtr, Ytr, Xte, Yte = du.loadData()
+
+print "Xtr shape: "
+print Xtr.shape
+print "Ytr shape: "
+print Ytr.shape
+print "Xte shape: "
+print Xte.shape
+print "Yte shape: "
+print Yte.shape
+
+#flatten out all images to be one-dimensional
+Xtr_rows = Xtr.reshape(Xtr.shape[0],1935*2592*3)
+Xte_rows = Xte.reshape(Xte.shape[0],1935*2592*3)
+
+nn = NearestNeighbour()
+nn.train(Xtr_rows, Ytr)
+Yte_predict = nn.predict(Xte_rows)
+
+print 'accuracy: %f' % (np.mean(Yte_predict == Yte))

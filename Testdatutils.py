546778d3c51c06d185453e3a57ff85d7c7fbd03c
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-09-27 16:32:19 +0100

    Reconfigure datutils, start seting up tests

---
+++ Testdatutils.py
@@ -0,0 +1,81 @@
+import unittest
+import datutils as dat
+import numpy as np
+
+
+class myTest(unittest.TestCase):
+
+    def setUp(self):
+        #setup some simpel 3x3 pcitures
+        self.pic1 = np.array([[1,2,3],[3,4,5],[6,7,8]])
+        self.pic2 = np.array([[1,2,3],[1,2,3],[1,2,3]])
+        self.pic3 = np.array([[3,4,5],[3,4,5],[3,4,5]])
+        self.pic4 = np.array([[7,8,9],[7,8,9],[7,8,9]])
+
+        self.traindata=[]
+        self.traindata.append(self.pic1)
+        self.traindata.append(self.pic2)
+        self.traindata.append(self.pic3)
+        self.traindata.append(self.pic4)
+
+        self.traindata = np.array(self.traindata)
+
+        self.pic5 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+        self.pic6 = np.array([[2,3,4],[5,6,7],[8,9,0]])
+        self.pic7 = np.array([[3,4,5],[6,7,8],[9,0,1]])
+
+        self.testdata=[]
+        self.testdata.append(self.pic5)
+        self.testdata.append(self.pic6)
+        self.testdata.append(self.pic7)
+
+        self.testdata = np.array(self.testdata)
+
+        self.du = dat.datutils()
+
+    def testMeanImg(self):
+        expected = np.array([[3.,4.,5.],[3.5,4.5,5.5],[4.25,5.25,6.25]])
+        self.failUnless(np.array_equal(expected,self.du.getMeanImg(self.traindata)))
+
+    def testsubMeanImg(self):
+        Xexpected = [[[-2.,-2.,-2.],[-0.5,-0.5,-0.5],[1.75,1.75,1.75]],[[-2.,-2.,-2.],[-2.5,-2.5,-2.5],[-3.25,-3.25,-3.25]],[[0.,0.,0.],[-0.5,-0.5,-0.5],[-1.25,-1.25,-1.25]],[[4.,4.,4.],[3.5,3.5,3.5],[2.75,2.75,2.75]]]
+        Yexpected = [[[-2.,-2.,-2.],[0.5,0.5,0.5],[2.75,2.75,2.75]],[[-1.,-1.,-1.],[1.5,1.5,1.5],[3.75,3.75,-6.25]],[[0.,0.,0.],[2.5,2.5,2.5],[4.75,-5.25,-5.25]]]
+
+        meanImg = np.asarray(self.du.getMeanImg(self.traindata))
+        Xsubmean = self.traindata - meanImg
+        Ysubmean = self.testdata - meanImg
+
+        self.failUnless(np.array_equal(Xexpected,Xsubmean))
+        self.failUnless(np.array_equal(Yexpected,Ysubmean))
+
+    def testaddBias(self):
+        meanImg=np.asarray(self.du.getMeanImg(self.traindata))
+        Xsubmean=self.traindata - meanImg
+        Ysubmean=self.testdata - meanImg
+
+        print Xsubmean.shape
+        print Ysubmean.shape
+
+        print "np.omnes..."
+
+        print np.ones((Xsubmean.shape[2],1))
+        print np.ones((Ysubmean.shape[2],1))
+
+#        self.failUnless(1==1)
+
+
+        Xtrain = np.hstack([Xsubmean,np.ones((Xsubmean.shape[2],1))])
+        Ytrain = np.hastack([Ysubmean,np.ones((Ysubmean.shape[2],1))])
+
+        print Xtrain
+        print Ytrain
+
+        
+
+
+
+
+
+if __name__ == '__main__': unittest.main()   
+
+        
--- datutils.py
+++ datutils.py
@@ -1,63 +1,104 @@
 import cPickle as pickle
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
-def loadTraining(ROOT,num):
+class datutils:
 
-    xs = []
-    ys = []
-    for i in range(1,num+1):
-        trainfname = os.path.join(ROOT,'train%d.pkl' % (i,))
-        labelsfname = os.path.join(ROOT,'indexes%d.pkl' % (i,))
-        print "Loading " + trainfname
-        with open(trainfname,'rb') as f:
-            X = pickle.load(f)
-            xs.append(X)
+    def __INIT__(self):
+        pass
 
-        print "Loading " + labelsfname
-        with open(labelsfname,'rb') as  f:
-            Y = pickle.load(f)
-            ys.append(Y)
+    def loadTraining(self,ROOT,num):
 
-    Xtr = np.concatenate(xs)
-    Ytr = np.concatenate(ys)
+        xs = []
+        ys = []
+        for i in range(1,num+1):
+            trainfname = os.path.join(ROOT,'train%d.pkl' % (i,))
+            labelsfname = os.path.join(ROOT,'indexes%d.pkl' % (i,))
+            print "Loading " + trainfname
+            with open(trainfname,'rb') as f:
+                X = pickle.load(f)
+                xs.append(X)
 
-    return Xtr,Ytr
+            print "Loading " + labelsfname
+            with open(labelsfname,'rb') as  f:
+                Y = pickle.load(f)
+                ys.append(Y)
+
+        Xtr = np.concatenate(xs)
+        Ytr = np.concatenate(ys)
+
+        return Xtr,Ytr
 
 
 
-def loadData(ROOT="",num=3):
+    def loadData(self,ROOT="",num=3):
 
-    X_tr, Y_tr = loadTraining(ROOT,num)
+        X_tr, Y_tr = self.loadTraining(ROOT,num)
 
-    filename=ROOT+"test.pkl"
+        filename=ROOT+"test.pkl"
 
-    print "Loading " + filename
-    with open(filename,'rb') as f:
-        X_te = pickle.load(f)
+        print "Loading " + filename
+        with open(filename,'rb') as f:
+            X_te = pickle.load(f)
 
-    X_te = np.array(X_te)
+        X_te = np.array(X_te)
     
-    filename=ROOT+"testinds.pkl"
+        filename=ROOT+"testinds.pkl"
     
-    print "Loading " + filename
-    with open(filename,'rb') as f:
-        Y_te = pickle.load(f)
+        print "Loading " + filename
+        with open(filename,'rb') as f:
+            Y_te = pickle.load(f)
     
-    Y_te = np.array(Y_te)
+        Y_te = np.array(Y_te)
 
-    return X_tr, Y_tr, X_te, Y_te
+        return X_tr, Y_tr, X_te, Y_te
 
-def flattenData(Xtr,Xte):
-    #flatten out all images to be one-dimensional
-#    Xtr_rows = Xtr.reshape(Xtr.shape[0],1935*2592*3)
-#    Xte_rows = Xte.reshape(Xte.shape[0],1935*2592*3)
-    Xtr_rows = Xtr.reshape(Xtr.shape[0],Xtr.shape[1]*Xtr.shape[2]*Xtr.shape[3])
-    Xte_rows = Xte.reshape(Xte.shape[0],Xte.shape[1]*Xte.shape[2]*Xte.shape[3])
+    def flattenData(self,Xtr,Xte):
+        #flatten out all images to be one-dimensional
+#       Xtr_rows = Xtr.reshape(Xtr.shape[0],1935*2592*3)
+#        Xte_rows = Xte.reshape(Xte.shape[0],1935*2592*3)
+        Xtr_rows = Xtr.reshape(Xtr.shape[0],Xtr.shape[1]*Xtr.shape[2]*Xtr.shape[3])
+        Xte_rows = Xte.reshape(Xte.shape[0],Xte.shape[1]*Xte.shape[2]*Xte.shape[3])
 
-    return Xtr_rows, Xte_rows
+        return Xtr_rows, Xte_rows
 
-def biasTrick(X_tr,X_te):
-    #append ones to each column of the image data
-    pass
+    def biasTrick(self,X_tr,X_te):
+        #append ones to each column of the image data
+        pass
+
+    def getMeanImg(self,Xtr):
+        meanImg = np.mean(Xtr, axis=0)
+        return meanImg
+
+    def showMeanImg(self,Xtr):
+        plt.figure(figsize=(4,4))
+        plt.imshow(self.getMeanImg(Xtr).reshape((Xtr.shape[1],Xtr.shape[2],Xtr.shape[3])).astype('uint8')) # visualize the mean image
+        plt.show()
+
+    def subMeanImg(self,Xtr,Ytr):
+        mean = aself.getMeanImg(Xtr)
+        Xtr -= mean
+        Ytr -= mean
+
+
+'''
+    def visualise(self):
+        Xtr,Ytr,Xte,Yte = self.loadData()
+        num_classes = len(set(Ytr))
+        samples_per_class = 3
+
+        for y, cls in enumerate(np.arange(num_classes)):
+            idxs = np.flatnonzero(Ytr==y)
+            idxs = np.random.choice(idxs, samples_per_class, replace=False) #ValueError: Cannot take a larger sample than population when 'replace=False'
+            for i, idx in enumerate(idxs):
+                plt_idx=i*num_classes+y+1
+                plt.subplot(samples_per_class, num_classes, plt_idx)
+                plt.imshow(Xtr[idx].astype('uint8'))
+                plt.axis("off")
+         plt.show()
+'''
+
+
+
 

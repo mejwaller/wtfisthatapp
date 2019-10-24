7aae64f2251d925502d595c6a0d58b03bd166106
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-10-24 15:48:04 +0100

    Transpose W, add regL2norm, etc.

--- CalcSVMLoss.py
+++ CalcSVMLoss.py
@@ -7,12 +7,21 @@ svm.loadData()
 
 W = svm.initScores(svm.Ytr,svm.Xtr_rows)
 
+print "transposing W - current shape is:"
+print W.shape
+W = W.transpose()
+print "W transposed shape:"
+print W.shape
+
 loss=0.
 
 tic=time.time()
 for i in xrange(svm.Xtr_rows.shape[0]):
     print "Loss for example %d" % i
     loss+=svm.L_i(svm.Xtr_rows[i],svm.Ytr[i],W)
+
+loss/=svm.Xtr_rows.shape[0]
+loss+=svm.regL2norm(W,1.)
 
 toc=time.time()
 print "Total loss:"
--- TestSVMLoss.py
+++ TestSVMLoss.py
@@ -15,7 +15,7 @@ class TestSVM(unittest.TestCase):
         self.failUnless(W.shape == expectedShape)
 
     def testScore(self):
-        W = np.eye(3,4)
+        W = np.eye(3,4).transpose()
         expected = np.array([[13,-7,11],[1,2,3]])
         """
         print "W:"
@@ -28,32 +28,29 @@ class TestSVM(unittest.TestCase):
         print self.svm.score(W,self.Xtr_rows)
         print "Done"
         """
-        self.failUnless(np.array_equal(expected.transpose().astype('float64'),self.svm.score(W,self.Xtr_rows)))
+        self.failUnless(np.array_equal(expected.astype('float64'),self.svm.score(W,self.Xtr_rows)))
 
+    
     def testL_i(self):
         x = np.array([13,-7,11,1])
-        W = np.eye(3,4)
-        y=0
+        W = np.eye(3,4).transpose()
+        y=np.array(0)
         expected = 0. #max(0,-7-13+1) + max(0,11-13+1) where the 1 is delta
         self.failUnless(expected==self.svm.L_i(x,y,W))
         x = np.array([1,2,3,1])
-        y=1
+        y=np.array(1)
         expected = 2. #max(0,1-2+1) + max(0,3-2+1)
         self.failUnless(expected==self.svm.L_i(x,y,W))
-
+    
     def testL_itot(self):
         loss=0.
-        expected = 2.0
-        W = np.eye(3,4)
+        expected = 2.5
+        W = np.eye(3,4).transpose()
         for i in xrange(self.Xtr_rows.shape[0]):
             loss+=self.svm.L_i(self.Xtr_rows[i],self.Ytr[i],W)
+        loss/=self.Xtr_rows.shape[0]
+        loss+=self.svm.regL2norm(W,1)
         self.failUnless(expected==loss)
-
-
-
-
-
-
-
+    
 
 if __name__ == '__main__': unittest.main()

cbe4b76bbb52a32cbd2d90e1345d732e88c54514
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-10-25 16:09:35 +0100

    Updates and better tests for vectorized loss

--- TestSVMLoss.py
+++ TestSVMLoss.py
@@ -6,12 +6,15 @@ class TestSVM(unittest.TestCase):
     def setUp(self):
         self.svm = SVMLoss.SVMLoss()
         self.Xtr_rows = np.array([[13,-7,11,1],[1,2,3,1]])
-        self.Ytr = np.array([0,1,1,1,2,2,2])
+        #self.Ytr = np.array([0,1,1,1,2,2,2])
+        self.Ytr = np.array([0,1])
 
 
     def testinitScores(self):
         W = self.svm.initScores(self.Ytr,self.Xtr_rows)
-        expectedShape=(3,4)
+        expectedShape=(2,4)
+        #print "testinitScores:"
+        #print W.shape
         self.failUnless(W.shape == expectedShape)
 
     def testScore(self):
@@ -44,13 +47,47 @@ class TestSVM(unittest.TestCase):
     
     def testL_itot(self):
         loss=0.
-        expected = 2.5
-        W = np.eye(3,4).transpose()
+        expected = 251.81625
+        
+        W = np.asarray([[.2,1.5,0.],[-.5,1.3,.25],[.1,2.1,.2],[2,0.,-.3],[1.1,3.2,-1.2]])
+
+        X = np.asarray([[56,231,24,2,1],[1,2,3,4,1],[4,3,2,1,1]])
+        
+        Y = np.asarray([0,1,2])
+
+        for i in xrange(X.shape[0]):
+            loss+=self.svm.L_i(np.asarray(X[i]),Y[i],W)
+        loss/=X.shape[0]
+
+        loss+=self.svm.regL2norm(W,1)
+
+        '''
+        W = np.eye(2,4).transpose()
         for i in xrange(self.Xtr_rows.shape[0]):
             loss+=self.svm.L_i(self.Xtr_rows[i],self.Ytr[i],W)
         loss/=self.Xtr_rows.shape[0]
         loss+=self.svm.regL2norm(W,1)
-        self.failUnless(expected==loss)
-    
+        '''
+        print "TESTLI_TOT acutls score is: %f" % loss
+
+        self.failUnless(np.isclose(expected,loss))
+
+    def test_vectorized(self):
+        loss=0.
+
+        loss=0.
+        expected = 251.81625
+        
+        W = np.asarray([[.2,1.5,0.],[-.5,1.3,.25],[.1,2.1,.2],[2,0.,-.3],[1.1,3.2,-1.2]])
+
+        X = np.asarray([[56,231,24,2,1],[1,2,3,4,1],[4,3,2,1,1]])
+        
+        Y = np.asarray([0,1,2])
+        
+        loss = self.svm.SVM_loss(X,Y,W)
+        print "testvectorized - actual loss is: %f" % loss
+        #print loss
+        self.failUnless(np.isclose(expected,loss))
+
 
 if __name__ == '__main__': unittest.main()

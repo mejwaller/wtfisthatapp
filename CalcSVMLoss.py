c9a9504ccea96dbbb1ff3940893d79de1b3ba3d8
ASSEENONSCREEN\martin.waller <martin.waller@asos.com>
2019-11-08 14:13:02 +0000

    grad calcs and SGD for linear classifier

--- CalcSVMLoss.py
+++ CalcSVMLoss.py
@@ -1,5 +1,6 @@
 import SVMLoss
 import time
+import numpy as np
 
 svm = SVMLoss.SVMLoss()
 
@@ -37,14 +38,37 @@ print "and it took %fs to run" % (toc-tic)
 
 print "Loss vectorized version:"
 loss=0.
-tic = time.time()
+#tic = time.time()
+step_size=2
+reg=1e-3
 
-loss = svm.SVM_loss(svm.Xtr_rows,svm.Ytr,W)
+delta=1e+9
 
-toc=time.time()
-print "Total loss:"
-print loss
-print "and it took %fs to run" % (toc-tic)
+scores = svm.score(W,svm.Xtr_rows)
+
+orig_loss = 1e+9
+
+while abs(delta) > 1:    
+
+    loss, grad, scores = svm.SVM_loss(svm.Xtr_rows,svm.Ytr,W,reg)
+
+    #toc=time.time()
+    print "Total loss:"
+    print loss
+    #print "and it took %fs to run" % (toc-tic)
+
+    predicted_class = np.argmax(scores, axis=1)    
+    print "Trainign accurcay: %f" % (np.mean(predicted_class == svm.Ytr.astype('int64')))
+
+    delta = orig_loss-loss
+
+    W+=-step_size*grad
+
+print "Predicted:"
+print np.argmax(scores, axis=1)
+print "Actual:"
+print svm.Ytr.astype('int64')
+
 
 '''
 print "Calculating gradient..."
